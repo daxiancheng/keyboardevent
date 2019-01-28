1090827c565cb5d42ea22f006a566bd9c059427a
daxiancheng <787129485@qq.com>
2019-01-29 00:16:26 +0800

    keyboardevent

---
+++ main.js
@@ -0,0 +1,31 @@
+var keys = {
+    '0':['q','w','e','r','t','y','u','i','o','p'],
+    '1':['a','s','d','f','g','h','j','k','l'],
+    '2':['z','x','c','v','b','n','m'],
+    'length':3
+}
+var hash = {
+    'q':'www.qq.com','w':'www.weibo.com','e':'www.ele.me','r':'www.renren.com','t':'lol.qq.com'
+}
+index0 = 0;
+while(index0 < keys['length']){
+    div1 = document.createElement('div');
+    wrapper.appendChild(div1);
+    row = keys[index0];
+    index1 = 0;
+    while( index1 < row['length']){
+        kbd1 = document.createElement('kbd');
+        kbd1.textContent = row[index1];
+        button1 = document.createElement('button');
+        button1.textContent = 'E'
+        div1.appendChild(kbd1);
+        kbd1.appendChild(button1);
+        index1 = index1+1;
+    }
+    index0 = index0+1;
+}
+document.onkeypress = function(cheng){
+    key = cheng['key'];
+    website = hash[key];
+    window.open('http://' + website, '_blank');
+}
